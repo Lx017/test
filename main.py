48ba338a5c8d6a56080b6504b651a318dd2ab8bb
Lx017 <dr_xiaol@163.com>
2021-10-17 02:55:30 -0400

    123

---
+++ main.py
@@ -0,0 +1,21 @@
+import os.path
+
+from torch.utils.data import Dataset
+from PIL import Image
+import cv2
+
+imgp="train/COCO_train2014_000000000009.jpg"
+img=Image.open(imgp)
+img.show()
+dir="/media/linxiao/Storage/udownload/"
+labeldir="train"
+os.path.join(dir,labeldir)
+
+class Mydattaa(Dataset):
+
+    def __init__(self, dir,lableDir):
+        self.dir=dir
+
+    def __getitem__(self, item):
+        self.dir = dir
+
